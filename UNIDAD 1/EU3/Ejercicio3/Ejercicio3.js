8cd6f77584e0981ff257f0a1aa900fbdebce2be4
dadodopa <dopazodomingues@gmail.com>
2025-09-29 09:25:38 +0200

    29/09

---
+++ UNIDAD 1/EU3/Ejercicio3/Ejercicio3.js
@@ -0,0 +1,14 @@
+
+    let respuesta = prompt("Introduce el número de las siguientes opciones:\n1. Potencia.\n2. Raíz.\n3.Redondeo.\n4. Trigonometría.");
+
+switch (respuesta) {
+    case 1:
+        document.getElementById("titulo").innerHTML="Potencia";
+        break;
+    case 2:
+    
+    break;
+    default:
+
+        break;
+}
